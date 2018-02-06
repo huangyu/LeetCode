bd39d805c5c2744c21d710225c58baa3c11b748a
huangyu0522 <384539643@qq.com>
2018-02-06 11:18:43 +0800

    提交MaximumDepthOfBinaryTree

---
+++ src/com/huangyu/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,15 @@
+package com.huangyu;
+
+import com.huangyu.bean.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+
+}
