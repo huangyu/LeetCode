03dc1a7c5bbb71cd64585a0fc626a3d3707ab136
huangyu0522 <384539643@qq.com>
2018-02-11 09:09:30 +0800

    MaxAreaOfIsland

---
+++ src/com/huangyu/MaxAreaOfIsland.java
@@ -0,0 +1,32 @@
+package com.huangyu;
+
+/**
+ * 695. Max Area of Island
+ * 
+ * @author huangyu
+ *
+ */
+public class MaxAreaOfIsland {
+
+	public int maxAreaOfIsland(int[][] grid) {
+		int maxArea = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 1) {
+					maxArea = Math.max(maxArea, getAreaOfIsland(grid, i, j));
+				}
+			}
+		}
+		return maxArea;
+	}
+
+	public int getAreaOfIsland(int[][] grid, int i, int j) {
+		if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+			grid[i][j] = 0;
+			return 1 + getAreaOfIsland(grid, i + 1, j) + getAreaOfIsland(grid, i - 1, j)
+					+ getAreaOfIsland(grid, i, j - 1) + getAreaOfIsland(grid, i, j + 1);
+		}
+		return 0;
+	}
+
+}
