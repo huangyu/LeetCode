500701d00c1b83462c0f4f26d7babe80c7cef29f
huangyu0522 <384539643@qq.com>
2018-02-23 11:39:15 +0800

    ReverseInteger

---
+++ src/com/huangyu/ReverseInteger.java
@@ -0,0 +1,26 @@
+package com.huangyu;
+
+/**
+ * 7. Reverse Integer
+ * 
+ * @author huangyu
+ *
+ */
+public class ReverseInteger {
+
+	public int reverse(int x) {
+		long result = 0;
+
+		while (x != 0) {
+			result = result * 10 + x % 10;
+			x /= 10;
+		}
+
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+			return 0;
+		}
+
+		return (int) result;
+	}
+
+}
