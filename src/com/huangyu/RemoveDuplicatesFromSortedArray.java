d0877a0899216d37f9461d1afc7dc66e7aa94a97
huangyu0522 <384539643@qq.com>
2018-02-28 11:31:07 +0800

    RemoveDuplicatesFromSortedArray

---
+++ src/com/huangyu/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,29 @@
+package com.huangyu;
+
+/**
+ * 26. Remove Duplicates from Sorted Array
+ * 
+ * @author huangyu
+ *
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+	public static void main(String[] args) {
+		System.out.println(removeDuplicates(new int[] { 1, 2, 3, 3, 4 }));
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		if (nums.length == 0 || nums.length == 1) {
+			return nums.length;
+		}
+
+		int result = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				result++;
+			}
+		}
+		return result;
+	}
+
+}
